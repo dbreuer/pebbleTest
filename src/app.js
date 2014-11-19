dd4e7ac92728f84798c581c6d4487e1ab079fad4
David Breuer <dbreuer83@gmail.com>
2014-11-19 07:49:49 +0000

    close to good

--- src/app.js
+++ src/app.js
@@ -3,32 +3,34 @@
  * author: David Breuer
  * licence: MIT
  */
-var Settings = require('settings');
+//var Settings = require('settings');
 var UI = require('ui');
-var Vector2 = require('vector2');
+//var Vector2 = require('vector2');
 
-Settings.config(
-  { url: 'http://www.davidbreuer.co.uk' },
-  function(e) {
-    console.log('closed configurable');
+var splashScreen = new UI.Card({ banner: 'images/budagame_big_icon.png' });
+splashScreen.show();
 
-    // Show the parsed response
-    console.log(JSON.stringify(e.options));
-
-    // Show the raw response if parsing failed
-    if (e.failed) {
-      console.log(e.response);
-    }
-  }
-);
-
-
-
-var wind = new UI.Window({ fullscreen: true });
-var image = new UI.Image({
-  position: new Vector2(0, 0),
-  size: new Vector2(144, 168),
-  image: 'images/budagame_big_icon.png'
+var mainScreen = new UI.Menu({
+  sections: [{
+    title: 'This is the begining',
+    items: [{
+      title: 'First Item',
+      subtitle: 'Some subtitle',
+      icon: 'images/budagame_icon.png'
+    }, {
+      title: 'Second item'
+    }]
+  }]
 });
-wind.add(image);
-wind.show();
+
+mainScreen.on('select', function(e) {
+  console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+  console.log('The item is titled "' + e.item.title + '"');
+});
+
+setTimeout(function() {
+  // Display the mainScreen
+  mainScreen.show();
+  // Hide the splashScreen to avoid showing it when the user press Back.
+  splashScreen.hide();
+}, 1000);
