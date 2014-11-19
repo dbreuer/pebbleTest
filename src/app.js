880de240e240fa3006fa9a67c684c0f94f2eb8bb
David Breuer <dbreuer83@gmail.com>
2014-11-19 07:13:17 +0000

    new

--- src/app.js
+++ src/app.js
@@ -3,85 +3,90 @@
  * author: David Breuer
  * licence: MIT
  */
-unction iconFromWeatherId(weatherId) {
-  if (weatherId < 600) {
-    return 2;
-  } else if (weatherId < 700) {
-    return 3;
-  } else if (weatherId > 800) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
+/**
+ * BudaGame test app
+ * author: David Breuer
+ * licence: MIT
+ */
+// We use the fake "PBL" symbol as default
+var defaultSymbol = "PBL";
+var symbol = defaultSymbol;
 
-function fetchWeather(latitude, longitude) {
+// Fetch stock data for a given stock symbol (NYSE or NASDAQ only) from markitondemand.com
+// & send the stock price back to the watch via app message
+// API documentation at http://dev.markitondemand.com/#doc
+function fetchStockQuote(symbol, isInitMsg) {
   var response;
   var req = new XMLHttpRequest();
-  req.open('GET', "http://api.openweathermap.org/data/2.1/find/city?" +
-    "lat=" + latitude + "&lon=" + longitude + "&cnt=1", true);
+  // build the GET request
+  req.open('GET', "http://dev.markitondemand.com/Api/Quote/json?" +
+    "symbol=" + symbol, true);
   req.onload = function(e) {
     if (req.readyState == 4) {
+      // 200 - HTTP OK
       if(req.status == 200) {
         console.log(req.responseText);
         response = JSON.parse(req.responseText);
-        var temperature, icon, city;
-        if (response && response.list && response.list.length > 0) {
-          var weatherResult = response.list[0];
-          temperature = Math.round(weatherResult.main.temp - 273.15);
-          icon = iconFromWeatherId(weatherResult.weather[0].id);
-          city = weatherResult.name;
-          console.log(temperature);
-          console.log(icon);
-          console.log(city);
+        var price;
+        if (response.Message) {
+          // the markitondemand API sends a response with a Message
+          // field when the symbol is not found
           Pebble.sendAppMessage({
-            "icon":icon,
-            "temperature":temperature + "\u00B0C",
-            "city":city});
+            "price": "Not Found"});
         }
+        if (response.Data) {
+          // data found, look for LastPrice
+          price = response.Data.LastPrice;
+          console.log(price);
 
+          var msg = {};
+          if (isInitMsg) {
+            msg.init = true;
+            msg.symbol = symbol;
+          }
+          msg.price = "$" + price.toString();
+          Pebble.sendAppMessage(msg);
+        }
       } else {
-        console.log("Error");
+        console.log("Request returned error code " + req.status.toString());
       }
     }
-  }
+  };
   req.send(null);
 }
 
-function locationSuccess(pos) {
-  var coordinates = pos.coords;
-  fetchWeather(coordinates.latitude, coordinates.longitude);
-}
-
-function locationError(err) {
-  console.warn('location error (' + err.code + '): ' + err.message);
-  Pebble.sendAppMessage({
-    "city":"Loc Unavailable",
-    "temperature":"N/A"
-  });
-}
-
-var locationOptions = { "timeout": 15000, "maximumAge": 60000 }; 
-
-
+// Set callback for the app ready event
 Pebble.addEventListener("ready",
                         function(e) {
-                          console.log("connect!" + e.ready);
-                          locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
+                          console.log("connect! " + e.ready);
                           console.log(e.type);
+                          // Fetch saved symbol from local storage (using
+                          // standard localStorage webAPI)
+                          symbol = localStorage.getItem("symbol");
+                          if (!symbol) {
+                            symbol = "PBL";
+                          }
+                          var isInitMsg = true;
+                          fetchStockQuote(symbol, isInitMsg);
                         });
 
+// Set callback for appmessage events
 Pebble.addEventListener("appmessage",
                         function(e) {
-                          window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-                          console.log(e.type);
-                          console.log(e.payload.temperature);
-                          console.log("message!");
+                          console.log("message");
+                          var isInitMsg;
+                          if (e.payload.init) {
+                            isInitMsg = true;
+                            fetchStockQuote(symbol, isInitMsg);
+                          }
+                          else if (e.payload.fetch) {
+                            isInitMsg = false;
+                            fetchStockQuote(symbol, isInitMsg);
+                          }
+                          else if (e.payload.symbol) {
+                            symbol = e.payload.symbol;
+                            localStorage.setItem("symbol", symbol);
+                            isInitMsg = false;
+                            fetchStockQuote(symbol, isInitMsg);
+                          }
                         });
-
-Pebble.addEventListener("webviewclosed",
-                                     function(e) {
-                                     console.log("webview closed");
-                                     console.log(e.type);
-                                     console.log(e.response);
-                                     });
