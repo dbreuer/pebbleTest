e96d6dca0f40ee9eb6351efb81cc79391e9d145d
David Breuer <dbreuer83@gmail.com>
2014-11-19 11:24:55 +0000

    train on move

--- src/app.js
+++ src/app.js
@@ -3,29 +3,38 @@
  * author: David Breuer
  * licence: MIT
  */
-//var Settings = require('settings');
-var UI = require('ui');
-//var Vector2 = require('vector2');
+  //var Settings = require('settings');
+  var UI = require('ui');
+  //var Vector2 = require('vector2');
+  //var Vibe = require('ui/vibe');
+  var splashScreen = new UI.Card({ banner: 'images/budagame_big_icon.png' });
 
-var splashScreen = new UI.Card({ banner: 'images/budagame_big_icon.png' });
-splashScreen.show();
+  splashScreen.show();
 
 var mainScreen = new UI.Menu({
   sections: [{
-    title: 'This is the begining',
+    title: 'HurryApp',
     items: [{
-      title: 'First Item',
-      subtitle: 'Some subtitle',
-      icon: 'images/budagame_icon.png'
+      title: 'Show next',
+      subtitle: 'Quick start'
     }, {
-      title: 'Second item'
+      title: 'Find next',
+      subtitle: 'Select route'
     }]
   }]
 });
 
 mainScreen.on('select', function(e) {
-  console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-  console.log('The item is titled "' + e.item.title + '"');
+  //console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+  //console.log('The item is titled "' + e.item.title + '"');
+  var content = "Your train XY is the in 8 minutes";
+  var detailCard = new UI.Card({
+    title:'Details',
+    subtitle:e.item.subtitle,
+    body: content
+  });
+  detailCard.show();
+
 });
 
 setTimeout(function() {
